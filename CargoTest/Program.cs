5173a4dae4cffb0ef9b5fdc48d22a70a5b360ca9
Seyma SARIGİL <Seyma SARIGL>
2017-04-08 16:41:28 +0300

    cargo

---
+++ CargoTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary;
+using System;
+using System.Threading;
+
+namespace CargoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CargoVehicle arac1 = new CargoVehicle("42AB1234");
+            CargoVehicle arac2 = new CargoVehicle("42AB1235", "Ford");
+
+            arac1.SpeedExceeded += new CargoVehicle.SpeedHandler(speed_limit);
+            arac2.SpeedExceeded += new CargoVehicle.SpeedHandler(speed_limit);
+            
+            for (byte i = 80; i < 130; i += 5)
+            {
+                arac1.Speed = i;
+                arac2.Speed = (byte)(i + 20);
+                Console.WriteLine(arac1.Plate + "'s speed = " + arac1.Speed);
+                Console.WriteLine(arac2.Plate + "'s speed = " + arac2.Speed);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static void speed_limit(CargoVehicle vehicle)
+        {
+            Console.WriteLine(vehicle.Plate + " has exceed limits on "+DateTime.Now+" with " + vehicle.Speed + " speed.");
+        }
+    }
+}
---
+++ CargoVehicle/CargoVehicle.cs
@@ -0,0 +1,63 @@
+﻿namespace ClassLibrary
+{
+    public class CargoVehicle
+    {
+        private string plate;
+        private byte speed;
+        private string brand = null;
+        const byte limit = 110;
+
+        public string Plate // Plaka 
+        {
+            get
+            {
+                return this.plate;
+            }
+            set
+            {
+                this.plate = value;
+            }
+        }
+
+        public byte Speed
+        {
+            get
+            {
+                return this.speed;
+            }
+            set
+            {
+                this.speed = value > 0 ? value : (byte)0;
+                if (this.speed >= limit)
+                    SpeedExceeded(this);
+            }
+        }
+
+        public string Brand //Marka
+        {
+            get
+            {
+                return this.brand;
+            }
+            set
+            {
+                this.brand = value;
+            }
+        }
+
+
+        /////////////// Constructor ////////////
+        public CargoVehicle(string plate)
+        {
+            this.Plate = plate;
+        }
+
+        public CargoVehicle(string plate, string brand) : this(plate)
+        {
+            this.Brand = brand;
+        }
+
+        public delegate void SpeedHandler(CargoVehicle vehicle);
+        public event SpeedHandler SpeedExceeded;
+    }
+}
